8ed951f9d6f9e22b1c237fc3c2c9b230823cb7eb
Gustavo Almeida <gustavoam916@gmail.com>
2022-08-16 18:30:17 -0300

    Usando o BigDecimal para ter resultados precisos

---
+++ src/testeTipos.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;;
+
+public class testeTipos 
+{
+    public static void main(String[] args)
+    {
+        BigDecimal num1 = new BigDecimal("2.3");
+        BigDecimal num2 = new BigDecimal("1.1");
+        BigDecimal resultado = num1.subtract(num2);
+        
+        //Meu Deus, tudo isso pra subtrair com precisão kkkkk
+        System.out.println(resultado);
+        System.out.println(resultado.toString());
+        System.out.println(2.3 - 1.1);
+    }
+
+}
