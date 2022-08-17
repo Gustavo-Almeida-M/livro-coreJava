b453865d4fc61afbdb532f8d5446cc129b8944ca
Gustavo Almeida <gustavoam916@gmail.com>
2022-08-17 17:20:05 -0300

    teste

---
+++ src/codigo3.java
@@ -0,0 +1,22 @@
+//import static java.lang.Math.sqrt;
+
+public class codigo3
+{
+    public static void main(String\u005B\u005D args)
+    {
+        // final double pi = 3.14;
+        // double x = 81;
+        // double y = Math.sqrt(x);
+        // System.out.println(y);
+
+        int valor1 = 123456789;
+        float valor2 = valor1;
+        double valor3 = valor1;
+
+        System.out.println(valor2);
+        System.out.println(valor3);
+    }
+    public static final double Pi = 3.14;
+}
+    
+
